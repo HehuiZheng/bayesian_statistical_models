64b2baef353b2503126203bb852f5c1038ef839f
Hehui Zheng <summerzheng.hehui@gmail.com>
2023-10-22 13:23:46 +0200

    plot results

--- experiments/soft_trunk_arm_brnn_exp.py
+++ experiments/soft_trunk_arm_brnn_exp.py
@@ -38,8 +38,46 @@ def load_data(path):
     return x_train, y_train
 
 
+def plot_3d(ys, preds_mean, save_fig=False, img_dir=None, img_name=None):
+    ax = plt.figure(figsize=(32,24)).add_subplot(projection='3d')
+    # ax.scatter(ys[i*100:(i+1)*100, 0], ys[i*100:(i+1)*100, 1], ys[i*100:(i+1)*100, 2], zdir='z', label='Data', color='red')
+    ax.scatter(ys[:, 0], ys[:, 1],ys[:, 2], zdir='z', label='Data P1', color='red')
+    ax.plot(preds_mean[:, 0], preds_mean[:, 1], preds_mean[:, 2], zdir='z', label='Mean P1', color='blue')
+    ax.scatter(ys[:, 6], ys[:, 7],ys[:, 8], zdir='z', label='Data 2', color='green')
+    ax.plot(preds_mean[:, 6], preds_mean[:, 7], preds_mean[:, 8], zdir='z', label='Mean P2', color='purple')
+    handles, labels = plt.gca().get_legend_handles_labels()
+    by_label = dict(zip(labels, handles))
+    ax.legend(by_label.values(), by_label.keys())
+    
+    if save_fig:
+        img_path = os.path.join(img_dir, img_name)
+        plt.savefig(img_path)
+        plt.close()
+    else:
+        plt.show()
+    return
+
+
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
+    SAVE_FIG = True
+    if SAVE_FIG:
+        import time
+        timestamp = time.time()
+        result_dir = os.path.join('../results/soft_trunk_arm', str(timestamp))
+        os.mkdir(result_dir)
+    else:
+        result_dir = None
+
+    log_training = False
+    
+    import wandb
+    if log_training:
+        wandb.init(
+            project='soft_arm_brnn',
+            group='test group',
+        )
+
 
     key = jr.PRNGKey(0)
     input_dim = 1
@@ -59,9 +97,11 @@ if __name__ == '__main__':
     y_raw = s_raw[:,1:,:]
 
     episode = x_raw.shape[0]
+
+
     x_train = []
     y_train = []
-    for i in range(episode):
+    for i in range(100):
         xs = x_raw[i][4:]
         ys = y_raw[i][4:]
         x_train.append(create_windowed_array(xs, window_size=window_size))
@@ -72,17 +112,21 @@ if __name__ == '__main__':
     print("Training data loaded")
 
 
+    ys_train = jnp.concatenate(y_raw[:100], axis=0)
+
+    s_raw_test = jnp.load(os.path.join('../data/trunk_arm/test', "x.npy"))
+    u_raw_test = jnp.load(os.path.join('../data/trunk_arm/test', "u.npy"))
+
+    x_raw_test = jnp.concatenate([s_raw_test[:,:-1,:], u_raw_test], axis=-1)
+    y_raw_test = s_raw_test[:,1:,:]
+    ys_test = jnp.concatenate(y_raw[100:], axis=0)
+
+    plot_3d(ys_train, ys_test, save_fig=SAVE_FIG, img_dir=result_dir, img_name='dataset.png')
+
+
     data_std = noise_level * jnp.ones(shape=(output_dim,))  # data_std
     data = Data(inputs=x_train, outputs=y_train)
-    
-    log_training = True
-    
-    import wandb
-    if log_training:
-        wandb.init(
-            project='soft_arm_brnn',
-            group='test group',
-        )
+
 
     print("Creating model")
 
@@ -96,6 +140,8 @@ if __name__ == '__main__':
     init_model_state = model.init(key=jr.PRNGKey(0))
     statistical_model_state = model.update(model_state=init_model_state, data=data)
 
+    print("Finish training")
+
     # Test on new data
     # x_test, y_test = load_data('../data/trunk_arm/test')
 
@@ -106,37 +152,64 @@ if __name__ == '__main__':
     y_raw_test = s_raw_test[:,1:,:]
 
 
-    SAVE_FIG = True
-    if SAVE_FIG:
-        import time
-        timestamp = time.time()
-        result_dir = os.path.join('../results/soft_trunk_arm', str(timestamp))
-        os.mkdir(result_dir)
+ 
+    # for i in range(x_raw_test.shape[0]):
+    #     test_xs = x_raw_test[i][4:]
+    #     test_ys = y_raw_test[i][4:]
 
-    for i in range(x_raw_test.shape[0]):
-        test_xs = x_raw_test[i][4:]
-        test_ys = y_raw_test[i][4:]
+    #     preds = vmap(model, in_axes=(0, None),
+    #              out_axes=StatisticalModelOutput(mean=0, epistemic_std=0, aleatoric_std=0,
+    #                                              statistical_model_state=None))(test_xs, statistical_model_state)
+        
+    #     ax = plt.figure(figsize=(32,24)).add_subplot(projection='3d')
+    #     # ax.scatter(test_ys[i*100:(i+1)*100, 0], test_ys[i*100:(i+1)*100, 1], test_ys[i*100:(i+1)*100, 2], zdir='z', label='Data', color='red')
+    #     ax.scatter(test_ys[:, 0], test_ys[:, 1],test_ys[:, 2], zdir='z', label='Data P1', color='red')
+    #     ax.plot(preds.mean[:, 0], preds.mean[:, 1], preds.mean[:, 2], zdir='z', label='Mean P1', color='blue')
+    #     ax.scatter(test_ys[:, 6], test_ys[:, 7],test_ys[:, 8], zdir='z', label='Data 2', color='green')
+    #     ax.plot(preds.mean[:, 6], preds.mean[:, 7], preds.mean[:, 8], zdir='z', label='Mean P2', color='purple')
+    #     handles, labels = plt.gca().get_legend_handles_labels()
+    #     by_label = dict(zip(labels, handles))
+    #     ax.legend(by_label.values(), by_label.keys())
+    #     # plt.show()
+    #     img_name = os.path.join(result_dir, str(i).zfill(5)+'.png')
+    #     if SAVE_FIG:
+    #         plt.savefig(img_name)
+    #         plt.close()
+    #     else:
+    #         plt.show()
+
+
+    for i in range(x_train.shape[0]):
+        train_xs = x_train[i]
+        train_ys = y_train[i]
+
+        preds = vmap(model, in_axes=(0, None),
+                 out_axes=StatisticalModelOutput(mean=0, epistemic_std=0, aleatoric_std=0,
+                                                 statistical_model_state=None))(train_xs, statistical_model_state)
+        plot_3d(train_ys, preds.mean, save_fig=SAVE_FIG, img_dir=result_dir, img_name="train" + str(i).zfill(5)+'.png')
+
+
+    x_test = []
+    y_test = []
+    for i in range(100, episode):
+        xs = x_raw[i][4:]
+        ys = y_raw[i][4:]
+        x_test.append(create_windowed_array(xs, window_size=window_size))
+        y_test.append(create_windowed_array(ys, window_size=window_size))
+    x_test = jnp.concatenate(x_test)
+    y_test = jnp.concatenate(y_test)
+    
+    for i in range(x_test.shape[0]):
+        test_xs = x_test[i]
+        test_ys = y_test[i]
 
         preds = vmap(model, in_axes=(0, None),
                  out_axes=StatisticalModelOutput(mean=0, epistemic_std=0, aleatoric_std=0,
                                                  statistical_model_state=None))(test_xs, statistical_model_state)
-        
-        ax = plt.figure(figsize=(32,24)).add_subplot(projection='3d')
-        # ax.scatter(test_ys[i*100:(i+1)*100, 0], test_ys[i*100:(i+1)*100, 1], test_ys[i*100:(i+1)*100, 2], zdir='z', label='Data', color='red')
-        ax.scatter(test_ys[:, 0], test_ys[:, 1],test_ys[:, 2], zdir='z', label='Data P1', color='red')
-        ax.plot(preds.mean[:, 0], preds.mean[:, 1], preds.mean[:, 2], zdir='z', label='Mean P1', color='blue')
-        ax.scatter(test_ys[:, 6], test_ys[:, 7],test_ys[:, 8], zdir='z', label='Data 2', color='green')
-        ax.plot(preds.mean[:, 6], preds.mean[:, 7], preds.mean[:, 8], zdir='z', label='Mean P2', color='purple')
-        handles, labels = plt.gca().get_legend_handles_labels()
-        by_label = dict(zip(labels, handles))
-        ax.legend(by_label.values(), by_label.keys())
-        # plt.show()
-        img_name = os.path.join(result_dir, str(i).zfill(5)+'.png')
-        if SAVE_FIG:
-            plt.savefig(img_name)
-            plt.close()
-        else:
-            plt.show()
+        plot_3d(train_ys, preds.mean, save_fig=SAVE_FIG, img_dir=result_dir, img_name="test" + str(i).zfill(5)+'.png')
+
+    
+
 
 
     # test_xs = jnp.concatenate(x_raw_test, axis=0)
@@ -174,13 +247,4 @@ if __name__ == '__main__':
     
 
 
-    # for i in range(test_xs.shape[0]//1000):
-    #     ax = plt.figure().add_subplot(projection='3d')
-    #     # ax.scatter(test_ys[i*100:(i+1)*100, 0], test_ys[i*100:(i+1)*100, 1], test_ys[i*100:(i+1)*100, 2], zdir='z', label='Data', color='red')
-    #     ax.scatter(test_ys[:, 0], test_ys[:, 1],test_ys[:, 2], zdir='z', label='Data', color='red')
-    #     ax.plot(test_xs[:, 0], test_xs[:, 1], test_xs[:, 2], zdir='z', label='Mean', color='blue')
-    #     handles, labels = plt.gca().get_legend_handles_labels()
-    #     by_label = dict(zip(labels, handles))
-    #     ax.legend(by_label.values(), by_label.keys())
-        plt.show()
         
